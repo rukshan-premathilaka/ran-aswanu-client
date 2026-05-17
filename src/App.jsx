da8d13890d70e7b40a8877cc6211bed79f5d819a
Rasintha_Rukshan <Rasintharukshanp@gmail.com>
2026-05-17 18:07:21 +0530

    ADD LIB: axios for http request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
+import {useState} from "react";
+import axios from 'axios'
 
 function App() {
+
+  const [userdata, setUserData] = useState()
+
+  const testApi = () => {
+    axios.get('http://localhost:8080/user')
+        .then(response => {
+          console.log(response)
+          setUserData(response.data)
+        })
+        .catch(error => {
+          console.error("There was an error fetching the data!", error)
+        })
+  }
+
 
   return (
 
     <div
         className="w-screen h-screen
         bg-lime-500
-        flex items-center justify-center"
+        flex flex-col items-center justify-center"
     >
 
       <header
@@ -15,6 +31,22 @@ function App() {
       >
         RUN ASWANNA CLIENT
       </header>
+      <button
+          className="p-5  bg-lime-50 text-lime-800 rounded-2xl font-medium cursor-pointer"
+          onClick={() => {testApi()}}
+      >API TEST</button>
+
+      <div>
+        {userdata ? (
+            <div className="font-medium text-lime-700 text-center mt-5">
+              <p>ID: {userdata.id}</p>
+              <p>Name: {userdata.name}</p>
+              <p>Email: {userdata.email}</p>
+            </div>
+        ) : (
+            <p className="font-medium text-lime-700 mt-5">No data yet. Click the button!</p>
+        )}
+      </div>
 
     </div>
 
