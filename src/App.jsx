e226d63e38dbf2f96a8c6fc2735615aa3be9dcda
rumeshhapugolla <rumeshrkhhapugolla@outlook.com>
2026-07-12 23:02:25 +0530

    chat page

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "@/page/login.jsx";
 import Welcome from "@/page/welcome.jsx";
 import SignIn from "@/page/signIn.jsx";
-import DashboardLayout from "@/layouts/Dashboard.jsx";
-import ProductClick from "@/page/ProductClick.jsx";
-import UserProfileSettings from "@/page/UserProfileSettings.jsx";
 
 function App() {
   return (
-      <UserProfileSettings/>
-
-
-      /*<Router>
+      <Router>
         <Routes>
-          {/!* The '/' path is home page *!/}
-          <Route path="/" element={<Welcome/>} />
+          {/* The '/' path is home page */}
+          <Route path="/" element={<Welcome />} />
 
-          {/!* Other routes *!/}
+          {/* Other routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<SignIn />} />
         </Routes>
-      </Router>*/
-
+      </Router>
   );
 }
 
