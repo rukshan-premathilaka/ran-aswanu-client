04a6c71853c3c4b06678ae64f9602ee5a43d42a6
rumeshhapugolla <rumeshrkhhapugolla@outlook.com>
2026-07-12 23:02:25 +0530

    chat page

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "@/page/login.jsx";
 import Welcome from "@/page/welcome.jsx";
 import SignIn from "@/page/signIn.jsx";
+import ChatPage from "@/page/ChatPage.jsx";
+import AuthLayout from "@/layouts/Authlayout.jsx";
 
 function App() {
   return (
-      <Router>
+      </>
+      /*<Router>
         <Routes>
-          {/* The '/' path is home page */}
+          {/!* The '/' path is home page *!/}
           <Route path="/" element={<Welcome />} />
 
-          {/* Other routes */}
+          {/!* Other routes *!/}
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<SignIn />} />
+
+
+            {/!* The '/' path is home page *!/}
+            <Route path="/chat" element={<ChatPage />} />
         </Routes>
-      </Router>
+      </Router>*/
   );
 }
 
